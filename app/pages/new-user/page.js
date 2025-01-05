b8c9e6a7772347d6e7b2cdece6d3a416dceb5321
BenNahum6 <83815124+BenNahum6@users.noreply.github.com>
2025-01-05 14:10:15 +0200

    הודעת commit שמתארת את השינויים שלך

--- app/pages/new-user/page.js
+++ app/pages/new-user/page.js
@@ -1,34 +1,33 @@
-// קומפוננטה נסיונית
-// // app/new-user/page.js
+// app/new-user/page.js
 
-// "use client"; // כדי לוודא שהקומפוננטה רצה בצד הלקוח
+"use client"; // כדי לוודא שהקומפוננטה רצה בצד הלקוח
 
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
-// async function fetchUsers() {
-//   try {
-//     const response = await fetch("/api/new_user", { 
-//       method: 'GET',
-//     });
+async function fetchUsers() {
+  try {
+    const response = await fetch("/api/new_user", {
+      method: 'GET',
+    });
 
-//     if (!response.ok) {
-//     //   throw new Error('Network response was not ok');
-//     }
+    if (!response.ok) {
+    //   throw new Error('Network response was not ok');
+    }
 
-//     const users = await response.json();
-//     console.log(users);
-//   } catch (error) {
-//     console.error('Error:', error);
-//   }
-// }
+    const users = await response.json();
+    console.log(users);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
 
-// export default function NewUser() {
-//   // הפעלת הפונקציה בתוך useEffect
-//   useEffect(() => {
-//     fetchUsers();
-//   }, []); // הרצה רק פעם אחת בעת טעינת הקומפוננטה
+export default function NewUser() {
+  // הפעלת הפונקציה בתוך useEffect
+  useEffect(() => {
+    fetchUsers();
+  }, []); // הרצה רק פעם אחת בעת טעינת הקומפוננטה
 
-//   return (
-//     <h1 className="text-red-500">Helooooooooooooooooo</h1>
-//   );
-// }
+  return (
+    <h1 className="text-red-500">Helooooooooooooooooo</h1>
+  );
+}
