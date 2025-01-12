07eab4776874074a95a5797cae6918fb73464277
BenNahum6 <83815124+BenNahum6@users.noreply.github.com>
2025-01-12 15:02:00 +0200

    fix key to database

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonserviceKey = process.env.NEXT_PUBLIC_SERVICE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonserviceKey);
 
 /*Check if the database connection exists*/
 const fetchAppointments = async () => {
