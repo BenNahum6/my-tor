bb114e0157b07667fe6bc66e180b033019ab8933
BenNahum6 <83815124+BenNahum6@users.noreply.github.com>
2025-01-12 14:38:24 +0200

    Add appointments time to page v0.3

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseServiceKey = process.env.NEXT_PUBLIC_SERVICE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
 /*Check if the database connection exists*/
 const fetchAppointments = async () => {
