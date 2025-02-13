dd1a02ea9fe21e8c7ae17b1edbe29a0a01ce5de7
BenNahum6 <83815124+BenNahum6@users.noreply.github.com>
2025-02-13 11:35:03 +0200

    Add authenticated to sign-up and sign-in and fix API

---
+++ app/api/auth/sign-in/route.js
@@ -0,0 +1,35 @@
+import { supabase } from '@/app/lib/supabase';
+import { NextResponse } from 'next/server';
+
+export async function POST(req) {
+    try {
+        const { email, password } = await req.json();
+
+        // ניסיון התחברות עם סיסמה
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if (error) {
+            console.error('Error during sign-in:', error);
+            return NextResponse.json(
+                { success: false, error: 'Unable to process the request' },
+                { status: 400 }
+            );
+        }
+
+        return NextResponse.json({
+            success: true,
+            message: 'User signed in successfully!',
+            user: data.user, // מחזיר את פרטי המשתמש (ללא סיסמה)
+            session: data.session, // מחזיר את ה-session (JWT token)
+        });
+    } catch (err) {
+        console.error('Unexpected error:', err);
+        return NextResponse.json(
+            { success: false, error: 'Unable to process the request' },
+            { status: 500 }
+        );
+    }
+}
